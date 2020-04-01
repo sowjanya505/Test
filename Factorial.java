832194fc63767747f6447c9abf75e5245507303f
sowjanya505 <62980183+sowjanya505@users.noreply.github.com>
2020-04-01 17:51:31 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        long f = 1;
+        for(int i = 1; i <= n; ++i)
+        {
+            
+            fact *= i;
+        }
+        System.out.printf("Factorial of %d = %d", n, fact);
+    }
+}
